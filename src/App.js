00849e32efc48959343b1bacb18834827933c95d
AkshayGengaje <akshay@ai4mtech.com>
2023-09-26 12:34:14 +0530

    Project - FarAway Delete Functionality Implemented

--- src/App.js
+++ src/App.js
@@ -10,11 +10,14 @@ export default function App() {
   function handleAddItems(item) {
     setItems((items) => [...items, item]);
   }
+  function handleDeleteItem(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
   return (
     <div className="app">
       <Logo />
       <Form onAddItems={handleAddItems} />
-      <PackingList items={items} />
+      <PackingList items={items} onDeleteItem={handleDeleteItem} />
       <Stats />
     </div>
   );
@@ -63,26 +66,26 @@ function Form({ onAddItems }) {
   );
 }
 
-function PackingList({ items }) {
+function PackingList({ items, onDeleteItem }) {
   return (
     <div className="list">
       <ul>
         {items.map((item) => {
-          return <List item={item} key={item.id} />;
+          return <List item={item} key={item.id} onDeleteItem={onDeleteItem} />;
         })}
       </ul>
     </div>
   );
 }
 
-function List({ item }) {
+function List({ item, onDeleteItem }) {
   return (
     <li>
       <span style={item.packed ? { textDecoration: "line-through" } : {}}>
-        {item.quantity}
+        {item.quantity} &nbsp;
         {item.description}
       </span>
-      <button>❌</button>
+      <button onClick={()=>onDeleteItem(item.id)}>❌</button>
     </li>
   );
 }
